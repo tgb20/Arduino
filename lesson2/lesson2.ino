3d0ab95245e38259bc57eb1c9b76bc52f239803b
Andrew Pasquale <andrew@elytra.net>
2016-08-21 20:13:46 -0400

    Use multi-line comments.

--- lesson2/lesson2.ino
+++ lesson2/lesson2.ino
@@ -1,29 +1,43 @@
-//We need to redeclare our pins
+/*
+ * Second Arduino project:
+ * Make the LED light flash
+ * 
+ * We need to declare our pins.
+ */
 
 int pin = 9;
 
-
 void setup() {
-  // put your setup code here, to run once:
-
-  //We need to set our pin to an output
+  /* 
+   * Put your setup code here to run once.
+   * We need to set our pin to an output.
+   */
   pinMode(pin,OUTPUT);
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //We want the LED to blink on and off
-  //We can use delays as well as alternating high and low outputs
-  //Start by turning the pin on
+  /* 
+   *  Put your main code here, to run repeatedly.
+   *  We want the LED to blink on and off.
+   *  We can use delays as well as alternating high and low outputs.
+   *  
+   *  Start by turning the pin on:
+   */
   digitalWrite(pin,HIGH);
-  //We then want to it to wait before doing the next item
-  delay(1000); //Every 1000 is equal to 1 second
-  //Now we can turn the LED off
+  
+  // Wait before doing the next item
+  delay(1000); // 1000 milliseconds is equal to 1 second
+  
+  // Turn the LED off
   digitalWrite(pin,LOW);
-  //And add another wait
+  
+  // Add another wait
   delay(1000);
-  //Our LED will now blink on and off every second.
-  //Because we are in a loop function it will go back to the top after the delay
-  //This will start the process over again
+  
+   /* 
+   *  Our LED will now blink on and off every second.
+   *  Because we are in a loop function it will go back
+   *  to the top of the loop after the delay.
+   *  This will start the process over again.
+   */
 }
