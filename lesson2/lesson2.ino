edf433775ba8564cbbf92e15b2bf547fc52daa66
Jacob Bashista <dev@tgb20.com>
2016-08-22 07:15:39 -0400

    Merge branch 'pr/1'
    # Conflicts:
    #	lesson3/lesson3.ino
    #	lesson4/lesson4.ino

--- lesson2/lesson2.ino
+++ lesson2/lesson2.ino
@@ -1,29 +1,46 @@
-//We need to redeclare our pins
+/*
+   Second Arduino project:
+   Make the LED light flash
 
-int pin = 9;
+   We need to declare our pins.
+*/
 
+int ledPin = 9;
 
 void setup() {
-  // put your setup code here, to run once:
-
-  //We need to set our pin to an output
-  pinMode(pin,OUTPUT);
-
+  /*
+     Put your setup code here to run once.
+     We need to set our pin to an output.
+  */
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //We want the LED to blink on and off
-  //We can use delays as well as alternating high and low outputs
-  //Start by turning the pin on
-  digitalWrite(pin,HIGH);
-  //We then want to it to wait before doing the next item
-  delay(1000); //Every 1000 is equal to 1 second
-  //Now we can turn the LED off
-  digitalWrite(pin,LOW);
-  //And add another wait
+  /*
+      Put your main code here, to run repeatedly.
+      We want the LED to blink on and off.
+      We can use delays as well as alternating high and low outputs.
+
+      Start by turning the pin on:
+  */
+  digitalWrite(ledPin, HIGH);
+
+  // Wait before doing the next item
+  delay(1000); // 1000 milliseconds is equal to 1 second
+
+  // Turn the LED off
+  digitalWrite(ledPin, LOW);
+
+  // Add another wait
   delay(1000);
-  //Our LED will now blink on and off every second.
-  //Because we are in a loop function it will go back to the top after the delay
-  //This will start the process over again
+
+  /*
+     Our LED will now blink on and off every second.
+     Because we are in a loop function it will go back
+     to the top of the loop after the delay.
+     This will start the process over again.
+
+     Experiment:
+     Try using different values for the delay time.
+  */
 }
--- lesson3/lesson3.ino
+++ lesson3/lesson3.ino
@@ -1,20 +1,22 @@
-//We need to declare our pins again
-//This time we will want three vars
-//One for the echo
-unsigned long echo = 0;
-//One for the sensors pin;
-int sensor = 10;
-//And one for the returning value
-unsigned long ultravalue = 0;
+/*
+   Use an Ultrasonic distance sensor
+
+   The HC-SR04 Ultrasonic sensor has a trigger pin and an echo pin.
+   Sending a signal to the trigger pin causes an ultrasonic
+   sound to be broadcast. We can detect how long it takes for the echo
+   to be heard and calculate the distance using the speed of sound.
+
+*/
+int echoPin = 7; // Listen for an echo on this pin
+int triggerPin = 8; // Generate an ultrasonic sound with this pin
+
+long duration, distance;
 
 void setup() {
-  //We are going to use some basic serial for seeing what our Arduino is doing
-  //We want it to output on the same rate as the monitor
+  // Use Serial to communicate from the Arduino.
   Serial.begin(9600);
- 
-  //We are using an Ultrasonic sensors which requires us to send data
-  pinMode(sensor,OUTPUT);
 
+<<<<<<< HEAD
 }
 
 //We need to create a custom value to hold the distance
@@ -30,16 +32,45 @@ unsigned long ping(){
   echo = pulseIn(ultraSoundSignal, HIGH); //Listen for echo
   ultraSoundSignal = (echo / 58.138) * .39; //convert to CM then to inches
   return sensor;
+=======
+  // Specify trigger and echo pins.
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+>>>>>>> pr/1
 }
 
 void loop() {
-  //We need a filler value to hold our ping when we get a message
-  int x = 0;
-  //Set the filler to the ping;
-  x = ping();
-  //The print the distance to the monitor
-  Serial.println(x);
-  //Wait 1/4 of a second to not overload the monitor
-  delay(250);
-  //You should now be getting information from the arudino!
+  // Send a reset signal to the trigger pin.
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+
+  // Generate an ultrasonic sound for 10 microseconds.
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+
+  // pulseIn measures the length of time (in microseconds) that
+  // it takes for the echo to be heard.
+  duration = pulseIn(echoPin, HIGH);
+
+  /*
+      Calculate the distance (in cm) based on the speed of sound.
+      In air, sound travels 340 m/s = 34,000 cm/s = 0.034 cm/microsecond
+
+      Elapsed time is for a round trip, so divide by 2:
+      0.034 cm/microsecond / 2 = 0.017 cm/microsecond
+  */
+  distance = duration * 0.017;
+  // Limit the distance to between 0 and 200 cm.
+  distance = constrain(distance, 0, 200);
+  // Print the distance to the serial port
+  Serial.println(distance);
+
+  // Wait 50 milliseconds to not overload the monitor
+  delay(50);
+
+  /*
+      Run this sketch and select the "Serial Monitor" from the Tools menu.
+      You should now be getting information from the Arduino!
+  */
 }
