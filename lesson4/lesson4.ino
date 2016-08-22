28f6c5fa49973e18cc4ad075f642cebd3b79a7c4
Andrew Pasquale <andrew@elytra.net>
2016-08-21 23:49:34 -0400

    Lesson 4 updates.

--- lesson4/lesson4.ino
+++ lesson4/lesson4.ino
@@ -1,62 +1,89 @@
-//THIS IS A CONTINUATION OF LESSON 3!
+/*
+   Use an Ultrasonic distance sensor, part 2
 
-//We need to declare our pins again
-//This time we will want three vars
-//One for the echo
-unsigned long echo = 0;
-//One for the sensors pin;
-int sensor = 10;
-//And one for the returning value
-unsigned long ultravalue = 0;
+   The HC-SR04 Ultrasonic sensor has a trigger pin and an echo pin.
+   Sending a signal to the trigger pin causes an ultrasonic
+   sound to be broadcast. We can detect how long it takes for the echo
+   to be heard and calculate the distance using the speed of sound.
+
+   This lesson adds a dimmable LED controlled by the distance sensor.
+*/
+int echoPin = 7; // Listen for an echo on this pin
+int triggerPin = 8; // Generate an ultrasonic sound with this pin
+
+long duration, distance;
+
+// NEW CODE HERE TO CONTROL LED
+
+// Add an LED
+int ledPin = 10;
+
+// Initialize LED brightness
+int brightness = 0;
 
 void setup() {
-  //We are going to use some basic serial for seeing what our Arduino is doing
-  //We want it to output on the same rate as the monitor
+  // Use Serial to communicate from the Arduino.
   Serial.begin(9600);
- 
-  //We are using an Ultrasonic sensors which requires us to send data
-  pinMode(sensor,OUTPUT);
 
-}
-
-//We need to create a custom value to hold the distance
-unsigned long ping(){
-  pinMode(ultraSoundSignal, OUTPUT); // Switch signalpin to output
-  digitalWrite(ultraSoundSignal, LOW); // Send low pulse 
-  delayMicroseconds(2); // Wait for 2 microseconds
-  digitalWrite(ultraSoundSignal, HIGH); // Send high pulse
-  delayMicroseconds(5); // Wait for 5 microseconds
-  digitalWrite(ultraSoundSignal, LOW); // Holdoff
-  pinMode(ultraSoundSignal, INPUT); // Switch signalpin to input
-  digitalWrite(ultraSoundSignal, HIGH); // Turn on pullup resistor
-  echo = pulseIn(ultraSoundSignal, HIGH); //Listen for echo
-  ultrasoundValue = (echo / 58.138) * .39; //convert to CM then to inches
-  return ultrasoundValue;
+  // Specify trigger and echo pins.
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  //We need a filler value to hold our ping when we get a message
-  int x = 0;
-  //Set the filler to the ping;
-  x = ping();
-  //The print the distance to the monitor
-  Serial.println(x);
+  // Send a reset signal to the trigger pin.
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
 
-  //THE NEW SECTION
+  // Generate an ultrasonic sound for 10 microseconds.
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
 
-  //We need to get the value from the sensor and map it to the brightness of the LED
-  //We are going to use a map function
-  //This takes one set of values and converts it to another
-  //It works like this y = map(input,oldMin, oldMax, newMin, newMax);
-  //We are using 0 and 10 for the oldMin and Max for 0 inches to 10 inches
-  //You can change this to match the distances you want
-  y = map(x,0,10,0,255);
-  //The y value is now on a scale of 0 to 255 which can be used to control an LED
-  //We want to write the value to the LED
-  digitalWrite(led,y);
-  //Your LED should now change brightness depending how far away your hand is from the sensor
-  
-  //Wait 1/4 of a second to not overload the monitor
-  delay(250);
-  //You should now be getting information from the arudino!  
+  // pulseIn measures the length of time (in microseconds) that
+  // it takes for the echo to be heard.
+  duration = pulseIn(echoPin, HIGH);
+
+  /*
+      Calculate the distance (in cm) based on the speed of sound.
+      In air, sound travels 340 m/s = 34,000 cm/s = 0.034 cm/microsecond
+
+      Elapsed time is for a round trip, so divide by 2:
+      0.034 cm/microsecond / 2 = 0.017 cm/microsecond
+  */
+  distance = duration * 0.017;
+
+  // Print the distance to the serial port
+  Serial.println(distance);
+
+  // Wait 50 milliseconds to not overload the monitor
+  delay(50);
+
+  /*
+      Run this sketch and select the "Serial Monitor" from the Tools menu.
+      You should now be getting information from the Arduino!
+  */
+
+  // NEW SECTION
+
+  // Limit the distance to between 0 and 200 cm.
+  distance = constrain(distance, 0, 200);
+
+  /*
+      Convert the distance into brightness values.
+
+      The map function takes one set of values and converts it to another
+      new_value = map(input, oldMin, oldMax, newMin, newMax);
+
+      Map distances from 0 to 200 cm to brightness from 255 to 0.
+      This makes the LED brightest when you are close and dims it
+      when the distance is greater.
+  */
+  brightness = map(distance, 0, 200, 255, 0);
+
+  // Write the brightness value to the LED.
+  analogWrite(ledPin, brightness);
+
+  // Print the brightness to the Serial port.
+  Serial.println(brightness);
 }
